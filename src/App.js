a61b97d98e77885df978869e56f2c02a0a63e488
S M Abdullah <s.m.abdullah.kst@gmail.com>
2021-04-27 14:14:21 +0600

    final commit for wpxpo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import './assets/css/bootstrap.min.css';
+import NoteTaking from "./components/NoteTaking";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NoteTaking/>
     </div>
   );
 }
